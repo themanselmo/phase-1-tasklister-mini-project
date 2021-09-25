6804fd38c18949a8fb119c23a05dd2a44037f1bc
max anselmo <itmanselmo@gmail.com>
2021-09-25 09:41:53 -0400

    completed assignment, updating a todo list by responding to a submit event on a form, capturing the input text and adding it to a list on the page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let subForm = document.getElementById('create-task-form');
+  let formElements = document.getElementsByTagName('input');
+  let textField = formElements[0];
+  let taskList = document.getElementById('tasks');
+
+  subForm.addEventListener('submit', (e) => {
+    
+    let enteredText = textField.value;
+    let newTask = document.createElement('li');
+    newTask.textContent = enteredText;
+    taskList.appendChild(newTask);
+
+    e.preventDefault();
+    
+  })
 });
